09831c934fc9f6c1d73289a58d3d4ba14b293259
emanuelevolpes <emanuele.volpes1@gmail.com>
2023-02-23 17:01:53 +0100

    first commit

---
+++ js/main.js
@@ -0,0 +1,12 @@
+'use strict';
+
+for (let i = 1; i <= 100; i++) {
+    console.log(i);
+    if (i % 3 == 0) {
+        console.log("fizz");
+     } else if (i % 5 == 0) {
+        console.log("buzz");
+     } else (i % 15 == 0); {
+        console.log("fizzbuzz");
+     };
+};
